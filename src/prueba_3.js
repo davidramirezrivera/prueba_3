361435d76309d908935dc2e01b1fac4c57434db7
david ramirez <davidramirezrivera2511@gmail.com>
2022-10-03 08:17:59 -0500

    completado

--- src/prueba_3.js
+++ src/prueba_3.js
@@ -1,12 +1,13 @@
-import fetch from "node-fetch";  
 const API = 'http://api.escuelajs.co/api/v1/products';  
 //const API = 'https://securetransfer.redsis.com/rest/forms/v1/teamGoAny/payload'
 
- const usua = document.getElementById('usuario');
+const usua = document.getElementById('usuario');
 const cont = document.getElementById('contraseña');
 const env = document.getElementById('enviar'); 
+const pub_post = document.getElementById('publicar_post');
+const pub_get = document.getElementById('publicar_get');
 
-env.addEventListener('click',get);
+//env.addEventListener('click',get);
 env.addEventListener('click',post);
 
 //GET //
@@ -19,8 +20,10 @@ function get(){
     fetchData('http://api.escuelajs.co/api/v1/products' ) 
     .then(response => response.json())
     .then(json => console.log(json))
+    .then(info =>pub_get.innerHTML= "<p />  se conecto a la api correctamente"+"<p/>")
     .catch(err => console.log (err))    
 };
+get();
 
 //POST //
 function post(){
@@ -48,66 +51,10 @@ function post(){
     }
     postData('http://api.escuelajs.co/api/v1/products',data)
     .then(response => response.json())
-    .then(data =>console.log(data))
+    .then(data => pub_post.innerHTML = "<p />Se envio la informacion exitosamente")
 }
 
-
-
-
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-/* import fetch from "node-fetch";  
-const API = 'http://api.escuelajs.co/api/v1/products';  
-//const API = 'https://securetransfer.redsis.com/rest/forms/v1/teamGoAny/payload'
-
-const usua = document.getElementById('usuario');
-const cont = document.getElementById('contraseña');
-const env = document.getElementById('enviar');
-
-env.addEventListener('click',fetchData);
-env.addEventListener('click',postData);
-
-//GET //
-function fetchData(urlApi){ 
-    return fetch(urlApi);
-} 
-
-fetchData(`${API}`) 
-.then(response => response.json())
-.then(json => console.log(json))
-.catch(err => console.log (err))
-
-//POST //
-
-function postData(urlApi, data){
-    const response = fetch(urlApi,{
-        method: 'POST',
-        node: 'corse', 
-        credentials: 'same-origin',
-        headers:{
-            'Content-Type': 'application/json' 
-        },
-        body: JSON.stringify(data)
-    });
-    return response;
-}
-
-const data = {
-    //"title": "Nintendo 3DS",
-    "title": usua,
-    "price": 300,
-    //"description": "consola de juegos fabricada por la compañia de nintendo",
-    "description": cont,
-    "categoryId": 5,
-    "images": ["https://images.pexels.com/photos/3702458/pexels-photo-3702458.jpeg?cs=srgb&dl=pexels-mike-esparza-3702458.jpg&fm=jpg"]
-}
-
- const data = {
+/* const data = {
     "user": "ingresoRedsis",
     "password": "Qwerty0909$"
-} 
-
-postData(`${API}`,data)
-.then(response => response.json())
-.then(data =>console.log(data)) */
+} */
